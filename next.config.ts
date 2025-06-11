a73e9f05ad8771ab162cd2e280c923738277379a
ghulamkarimi <karimiamirhossein12@gmail.com>
2025-06-11 16:11:55 +0200

    Refactor next.config.ts: enhance configuration structure and maintain output settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+import type { NextConfig } from 'next';
+const nextConfig = {
+  output: 'standalone',
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+        },
+        {
+          key: 'Pragma',
+          value: 'no-cache',
+        },
+        {
+          key: 'Expires',
+          value: '0',
+        },
+      ],
+    },
+  ],
 };
-
 export default nextConfig;
